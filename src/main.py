467465a5ad5c90baaa3fb2ce73a76042f0d1c850
jrcaro <caroromerojuan@gmail.com>
2026-07-07 21:56:05 +0200

    Decathlo planning to Google calendar

---
+++ src/main.py
@@ -0,0 +1,184 @@
+import logging
+import requests
+import time
+import json
+from datetime import datetime
+from dateutil.rrule import rrule, MONTHLY
+from constants import *
+from utils import *
+import argparse
+import sys
+
+DEBUG = False
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+    datefmt="%m-%d-%Y %H:%M:%S",
+)
+logger = logging.getLogger("DecathlonToCalendar")
+
+def main(
+   user,
+   password,
+   calendar_id,
+   collaborator_id     
+):
+    # Selenium driver
+    logger.info("Create selenium driver")
+    driver = get_driver(DEBUG)
+    logger.info("Go to login endpoint")
+    driver.get(LOGIN_ENDPOINT)
+    time.sleep(2)
+
+    # Login
+    logger.info("Send user and password")
+    driver.find_element(By.ID, "username").send_keys(user)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID, "cnxbton").click()
+    time.sleep(2)
+
+    # Cookies
+    if DEBUG:
+        driver.find_element(By.ID, "didomi-notice-agree-button").click()
+
+    # Get token
+    logger.info("Get authentication header")
+    logs = driver.get_log("performance")
+    sel_requests = [json.loads(lr["message"])["message"] for lr in logs]
+    sel_headers = list(filter(log_filter, sel_requests))[0]["params"]["request"]["headers"]
+
+    # Get contract end date
+    logger.info("Call collaborator endpoint")
+    res_contract = requests.get(f"{COLLABORATOR_ENDPOINT}/{collaborator_id}", headers=sel_headers)
+    if res_contract.status_code:
+        end_contract_date = "-".join(res_contract.json()["activeContract"]["endDate"].split("-")[0:2]) + "-01"
+    else:
+        raise Exception
+
+    logger.info("Get available dates")
+    planning_dates_ = [date for date in rrule(MONTHLY, dtstart=datetime.now(), until=datetime.strptime(end_contract_date,"%Y-%m-%d"))]
+    planning_dates_.append(datetime.strptime(end_contract_date,"%Y-%m-%d"))
+    planning_dates = [datetime.strftime(x,'%Y-%m')+"-01" for x in planning_dates_]
+    collaborator_periods__ = None
+
+    # Get all periods
+    for month_filter in planning_dates:
+        # Call Planning API
+        logger.info(f"Call planning endpoint -> {month_filter}")
+        res = requests.get(PLANNING_ENDPOINT, headers=sel_headers, params={
+            "filter[month]": month_filter,
+            "filter[collaboratorUuid]": collaborator_id
+        })
+        
+        if res.status_code:
+            # Extend to list
+            if not collaborator_periods__:
+                collaborator_periods__ = res.json()["collaboratorPeriods"]
+            else:
+                collaborator_periods__.extend(res.json()["collaboratorPeriods"])
+
+    # Delete duplicates
+    collaborator_periods_ = [dict(t) for t in {tuple(d.items()) for d in collaborator_periods__}]
+    collaborator_periods = list(filter(lambda x: datetime.strptime(x["fromHour"],"%Y-%m-%dT%H:%M:%S") > datetime.now(),collaborator_periods_))
+
+    # Get all Google events
+    tmin = datetime.strftime(datetime.now(),"%Y-%m-%dT%H:%M:%SZ")
+    tmax = datetime.strftime(max([datetime.strptime(x["fromHour"],"%Y-%m-%dT%H:%M:%S") for x in collaborator_periods]),"%Y-%m-%dT%H:%M:%SZ")
+    service = calendar_service()
+
+    logger.info("Get actual Google calendar events")
+    
+    eventsResult = service.events().list(
+        calendarId=calendar_id,
+        timeMin=tmin,
+        timeMax=tmax,
+        singleEvents=True,
+        orderBy='startTime',
+        timeZone='Europe/Madrid'
+    ).execute()
+
+    # Data key
+    google_events = eventsResult.get("items", [])
+    decathlon_uuid_periods = [x["planningPeriodUuid"] for x in collaborator_periods]
+
+    # Google periods UUIDs
+    if google_events:
+        try:
+            google_uuid_periods = [x["description"] for x in google_events]
+        except KeyError:
+            google_uuid_periods = []
+    else:
+        google_uuid_periods = []
+            
+    logger.info("Compare UUIDs")
+    diff_uuids_google = list(set(google_uuid_periods)-(set(decathlon_uuid_periods)))
+    diff_uuids_decathlon = list((set(decathlon_uuid_periods))-set(google_uuid_periods))
+
+    if not diff_uuids_google and not diff_uuids_decathlon:
+        logger.info("No new periods")
+        sys.exit()
+
+    # Insert all periods when UUID is not in Google
+    for decathlon_uuid in diff_uuids_decathlon:
+        decathlon_event = list(filter(lambda x: uuid_filter(x,decathlon_uuid),collaborator_periods))[0]                
+        decathlon_datime_start = datetime.strptime(decathlon_event["fromHour"],"%Y-%m-%dT%H:%M:%S")
+        decathlon_datime_end = datetime.strptime(decathlon_event["toHour"],"%Y-%m-%dT%H:%M:%S")
+        summary = SECTION[decathlon_event["abbreviation"]]
+
+        # Body
+        event = {
+            'summary': summary,
+            'location': 'Cam. Loma de San Julián, Malaga',
+            'description': decathlon_event["planningPeriodUuid"],
+            'start': {
+                'dateTime': datetime.strftime(decathlon_datime_start,"%Y-%m-%dT%H:%M:%S"),
+                'timeZone': 'Europe/Madrid',
+            },
+            'end': {
+                'dateTime': datetime.strftime(decathlon_datime_end,"%Y-%m-%dT%H:%M:%S"),
+                'timeZone': 'Europe/Madrid',
+            },
+            'reminders': {
+                'useDefault': True
+            },
+        }
+
+        logger.info(f"Inserting {datetime.strptime(decathlon_event["fromHour"],"%Y-%m-%dT%H:%M:%S").date()}")
+        service.events().insert(calendarId=calendar_id, body=event).execute()
+
+    # Delete all periods when UUID is not in collaborator
+    for google_uuid in diff_uuids_google:
+        google_event = list(filter(lambda x: uuid_filter(x,google_uuid),google_events))[0]
+        logger.info(f"Deleting {datetime.strptime(decathlon_event["start"]["datetime"],"%Y-%m-%dT%H:%M:%S").date()}")
+        service.events().delete(calendarId=calendar_id, eventId=google_event['id']).execute()
+
+if __name__ == "__main__":
+    """
+    python src/main.py
+     --user your.email@mail.com
+     --password 1234
+     --calendar-id <calendar_id>@group.calendar.google.com
+     --collaborator-id
+    """
+    parser = argparse.ArgumentParser(
+        description="CLI tool to copy Decathlon planning to Google calendar"
+    )
+
+    parser.add_argument(
+        "--user", required=True, type=str, help="Decathlon planning user name"
+    )
+    parser.add_argument(
+        "--password", required=True, type=str, help="Decathlon planning password"
+    )
+    parser.add_argument(
+        "--calendar-id", required=True, type=str, help="Google calendar ID"
+    )
+    parser.add_argument(
+        "--collaborator-id", required=True, type=str, help="Decathlon collaborator ID"
+    )
+
+    args = parser.parse_args()
+    input = {key: value for key, value in args.__dict__.items() if value is not None}
+    main(**input)
---
+++ src/utils.py
@@ -0,0 +1,99 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.auth.identity_pool import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from constants import SCOPES
+import os
+import logging
+
+def calendar_service():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    try:
+        # Github execution
+        creds = Credentials.from_file(
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+        scopes=SCOPES
+    )
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+    except KeyError:
+        # Local execution
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials_2526.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+    try:
+        service = build("calendar", "v3", credentials=creds)
+    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+    
+    return service
+
+
+def get_driver(debug):
+    # The log disable with the option headless=new is not working
+    logging.getLogger('selenium').setLevel(logging.ERROR)
+
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+    chrome_options = Options()
+    options = [
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    if not debug:
+        chrome_options.add_argument("--headless=new")
+
+    return webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+def log_filter(log_):
+    return (
+        log_["method"] == "Network.requestWillBeSent"
+        and "Authorization" in log_["params"]["request"]["headers"]
+    )
+
+def uuid_filter(event_, uuid_):
+    if "planningPeriodUuid" in event_.keys():
+        return (
+            event_["planningPeriodUuid"] == uuid_
+        )
+    else:
+        return (
+            event_["description"] == uuid_
+        )
